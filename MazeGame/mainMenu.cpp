1a9153f31f2e4d5fa9bd519a06d4aac4e25aa5da
VNTeneva19 <vnteneva19@codingburgas.bg>
2021-10-24 16:19:11 +0300

    Add new colors to main menu

--- MazeGame/mainMenu.cpp
+++ MazeGame/mainMenu.cpp
@@ -9,128 +9,107 @@ using namespace std;
 bool exitProgram = true;
 void color(int color)
 {
-    if (SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color)) { return; }
-    else {}
+	if (SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color)) { return; }
+	else {}
 }
 
 void gotoxy(int x, int y)
 {
-    COORD c;
-    c.X = x;
-    c.Y = y;
-    if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c)) { return; }
-    else {}
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	if (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c)) { return; }
+	else {}
 }
 
 void mainMenu()
 {
 
-    int Set[] = { 7,7,7 };
-    int counter = 3;
-    char key;
-    cout << endl;
-    cout << endl;
-    cout << "        _Main menu_" << endl;
+	int Set[] = { 7,7,7 };
+	int counter = 3;
+	char key;
+	cout << endl;
+	cout << endl;
+
+	cout << "        __  __     __   __     ______   __     ______   __         ______     _____    " << endl;
+	cout << "       /\\ \\/\\ \\   /\\ "" -.\\ \\  / \\__  _\\ /\\ \\   /\\__  _\\ /\\ \\       /\\  ___\\   /\\  __-.  " << endl;
+	cout << "       \\ \\ \\_\\ \\  \\ \\ \\-.  \\ \\/_/\\ \\ / \\ \\ \\  \\/_/\\ \\/ \\ \\ \\____  \\ \\  __\\   \\ \\ \\/\\ \\ " << endl;
+	cout << "        \\ \\_____\\  \\ \\_\\ \\""\\_\\   \\ \\_\\   \\ \\_\\    \\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\____- " << endl;
+	cout << "         \\/_____/   \\/_/ \\/_/    \\/_/    \\/_/     \\/_/   \\/_____/   \\/_____/   \\/____/ " << endl;
+
+	do
+	{
 
 
-    cout << "       |           |   __  __     __   __     ______   __     ______   __         ______     _____    " << endl;
-    cout << "       |           |  /\\ \\/\\ \\   /\\ "" -.\\ \\  / \\__  _\\ /\\ \\   /\\__  _\\ /\\ \\       /\\  ___\\   /\\  __-.  " << endl;
-    cout << "       |           |  \\ \\ \\_\\ \\  \\ \\ \\-.  \\ \\/_/\\ \\ / \\ \\ \\  \\/_/\\ \\/ \\ \\ \\____  \\ \\  __\\   \\ \\ \\/\\ \\ " << endl;
-    cout << "       |           |   \\ \\_____\\  \\ \\_\\ \\""\\_\\   \\ \\_\\   \\ \\_\\    \\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\____- " << endl;
-    cout << "       |           |    \\/_____/   \\/_/ \\/_/    \\/_/    \\/_/     \\/_/   \\/_____/   \\/_____/   \\/____/ " << endl;
-    cout << "       |           |" << endl;
-    cout << "       |___________|" << endl;
+		gotoxy(5, 10);
+		color(Set[0]);
+		cout << "1. Play";
 
-    do
-    {
+		gotoxy(5, 11);
+		color(Set[1]);
+		cout << "2. Rules";
 
-
-        gotoxy(10, 5);
-        color(Set[0]);
-        cout << "1. Play";
-
-        gotoxy(10, 6);
-        color(Set[1]);
-        cout << "2. Help";
-
-        gotoxy(10, 7);
-        color(Set[2]);
-        cout << "3. Exit";
+		gotoxy(5, 12);
+		color(Set[2]);
+		cout << "3. Exit";
 
 
 
-        key = _getch();
+		key = _getch();
 
-        if (key == 'w' && (counter >= 2 && counter <= 5))
-        {
-            counter--;
-        }
-        if (key == 's' && (counter >= 1 && counter <= 4))
-        {
-            counter++;
-        }
-        if (key == '\r')
-        {
-            if (counter == 1)
-            {
+		if (key == 'w' && (counter >= 2 && counter <= 5))
+		{
+			counter--;
+		}
+		if (key == 's' && (counter >= 1 && counter <= 4))
+		{
+			counter++;
+		}
+		if (key == '\r')
+		{
+			if (counter == 1)
+			{
+				exitProgram = true;
+					system("CLS");
+					mazeDeclaration();
+			}
+			if (counter == 2)
+			{
+				exitProgram = true;
 
-                exitProgram = true;
-                if (key == 'w' && (counter >= 2 && counter <= 5))
-                {
-                    counter--;
-                }
-                if (key == 's' && (counter >= 1 && counter <= 4))
-                {
-                    counter++;
-                }
-                if (key == '\r')
-                {
-                    system("CLS");
-                    mazeDeclaration();
-                }
+			}
+			if (counter == 3)
+			{
+				exitProgram = false;
 
-            }
-            if (counter == 2)
-            {
-                exitProgram = true;
+			}
 
-            }
-            if (counter == 3)
-            {
-                exitProgram = false;
+		}
 
-            }
+		Set[0] = 7;
+		Set[1] = 7;
+		Set[2] = 7;
 
-        }
+		if (counter == 1)
+		{
+			Set[0] = 10;
 
-        Set[0] = 7;
-        Set[1] = 7;
-        Set[2] = 7;
-
-
-
-
-
-        if (counter == 1)
-        {
-            Set[0] = 12;
-
-        }
-        if (counter == 2)
-        {
-            Set[1] = 12;
-        }
-        if (counter == 3)
-        {
-            Set[2] = 12;
-        }
+		}
+		if (counter == 2)
+		{
+			Set[1] = 3;
+		}
+		if (counter == 3)
+		{
+			Set[2] = 4;
+		}
 
 
 
-    } while (exitProgram != false);
+	} while (exitProgram != false);
 
 
-    system("cls");
+	system("cls");
 
 
 
