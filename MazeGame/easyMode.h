6e3e79e8366ae35471229680218badf8af069a6e
Mariq <mzilcheva19@codingburgas.bg>
2021-10-31 01:52:37 +0300

    Change the sign for the maze walls

--- MazeGame/easyMode.h
+++ MazeGame/easyMode.h
@@ -3,4 +3,9 @@
 void printMaze(char** mazeBoard, int keysPressedCounter);
 void movementSystem(bool notWin, char** mazeBoard);
 void winMessage();
+void resetArray(char** arr);
+int isInBounds(int x, int y);
+void cleanTunnels(int x, int y, char** arr);
+void printMaze(char** arr);
+void easyMode();
 void mazeDeclaration();
